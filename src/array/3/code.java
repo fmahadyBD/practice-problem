dbbfb292f5ff19a8b439cd5ec7d4222dc9c77b2a
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2024-07-10 16:38:40 +0200

    fixed the code.java

--- src/array/3/code.java
+++ src/array/3/code.java
@@ -1,10 +1,13 @@
-public class code {
-    public int maxSubArray(int[] nums) {
+class Solution {
+     public int maxSubArray(int[] nums) {
         int sum=Integer.MIN_VALUE;
         int max=Integer.MIN_VALUE;
+        
         for(int i=0;i<nums.length;i++){
             if(sum<0){
                 sum=nums[i];
+            }else{
+                sum=sum+nums[i];
             }
             max=Math.max(max,sum);
             
